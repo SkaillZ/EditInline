8dcdea39149ea7b2f00bf9a5a406a12d992b7574
Rene Buchmayer <rene.buchmayer@gmail.com>
2016-12-16 16:34:07 +0100

    Add files for first version

---
+++ Assets/InlineObjectEditor/Sample/Scripts/Weapon.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(order = 999999)]
+public class Weapon : ScriptableObject
+{
+    public string weaponName;
+    public int attack = 2;
+    public int defense = 1;
+}
